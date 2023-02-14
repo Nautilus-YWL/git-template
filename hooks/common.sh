0c55531e07ff1d69191c3b528cfe15a69e3ed37c
GinShio <ginshio78@gmail.com>
2023-02-14 13:38:54 +0800

    refactor the hooks

--- hooks/common.sh
+++ hooks/common.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+export GIT_COMMON_DIR=$(git rev-parse --git-common-dir)
+export CURRENT_BRANCH=$(git branch --show-current) # git 2.22+
+# export CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export PROTECTED_BRANCH='^(master|dev|release-*|patch-*)'
+export FORCE_PUSH_CMD='force|delete|-f'
+
 test -d "$GIT_COMMON_DIR"/rebase-merge -o -d "$GIT_COMMON_DIR"/rebase-apply && exit 0
 
 function find-program {
@@ -10,3 +16,22 @@ function find-program {
     }
 }
 export -f find-program
+
+function prompt-to-confirm {
+    local status=0
+    exec </dev/tty
+    while true; do
+        read -r -p "Are you sure want to continue? [Y/n] " input
+        case $input in
+            [Yy][Ee][Ss]|[Yy])
+                break;;
+            [Nn][Oo]|[Nn])
+                local status=1
+                break;;
+            *)
+        esac
+    done
+    exec 0<&-
+    return $status
+}
+export -f prompt-to-confirm
