19a9dab96f37223b1209858f741c704da4c169c9
GinShio <ginshio78@gmail.com>
2023-02-23 01:30:18 +0800

    update emacs config

--- hooks/editor/emacs-post-checkout.sh
+++ hooks/editor/emacs-post-checkout.sh
@@ -5,8 +5,9 @@ if [[ $(find-program emacs >/dev/null 2>&1) -ne 0 || -f "$GIT_TOPLEVEL_DIR"/.dir
 fi
 
 cat <<-EOF |tee "$GIT_TOPLEVEL_DIR"/.dir-locals.el
-(
- (format-all-formatters
-  ("C++" clang-format "--Wno-error=unknown"))
+;; -*- mode: emacs-lisp; lexical-binding: t; -*-
+((nil
+  (format-all-formatters
+   ("C++" (clang-format "--Wno-error=unknown"))))
  )
 EOF
